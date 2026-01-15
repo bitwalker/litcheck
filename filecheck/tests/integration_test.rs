9d13beb12bf3183f7f794d5d077a022a29cb95dc
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2026-01-15 13:57:48 -0500

    test: add integration tests for variable binding/scoping modes

--- filecheck/tests/integration_test.rs
+++ filecheck/tests/integration_test.rs
@@ -280,3 +280,180 @@ fn integration_test_check_dag_xfail_x6() -> DiagResult<()> {
         Err(error.into())
     }
 }
+
+#[test]
+fn midenc_hir_example_with_local_bindings_and_var_scoping_disabled() -> DiagResult<()> {
+    const SOURCE: &str = "\
+public builtin.function @test::spill_nested_scf_if(v0: ptr<element, u8>) -> u32 {
+^block1(v0: ptr<element, u8>):
+     v2 = hir.ptr_to_int v0 : u32;
+     v3 = arith.constant 32 : u32;
+     v4 = arith.add v2, v3 : u32 #[overflow = unchecked];
+     v5 = hir.int_to_ptr v4 : ptr<element, u128>;
+     v6 = hir.load v5 : u128;
+     v7 = arith.constant 64 : u32;
+     v8 = arith.add v2, v7 : u32 #[overflow = unchecked];
+     v9 = hir.int_to_ptr v8 : ptr<element, u128>;
+     v10 = hir.load v9 : u128;
+     v11 = arith.constant 1 : u64;
+     hir.store_local v5 #[local = lv0];
+     v12 = hir.exec @test/example(v9, v6, v10, v10, v11) : u32
+     v13 = arith.constant 0 : u32;
+     v14 = arith.eq v12, v13 : i1;
+     v15 = scf.if v14 : u32 {
+     ^block3:
+         v18 = hir.load_local  : ptr<element, u128> #[local = lv0];
+         hir.store v18, v4;
+         scf.yield v12;
+     } else {
+     ^block4:
+         v19 = hir.load_local  : ptr<element, u128> #[local = lv0];
+         hir.store v19, v4;
+         scf.yield v12;
+     };
+     cf.br ^block2(v15);
+ ^block2(v1: u32):
+     builtin.ret v1;
+};
+";
+    const CHECKS: &str = r#"; COM: Spill of v5
+; CHECK:     hir.store_local v5 #[local = [[L:lv[0-9]+]]];
+; CHECK-NEXT:     {{v[0-9]+}} = hir.exec @test/example
+
+; COM: First reload of v5
+; CHECK-LABEL:     ^block3
+; CHECK:         [[R1:v[0-9]+]] = hir.load_local  : ptr<element, u128> #[local = [[L]]];
+; CHECK-NEXT:         hir.store [[R1]], v[[#]]
+
+; COM: Second reload of v5
+; CHECK-LABEL:     ^block4
+; CHECK:         [[R2:v[0-9]+]] = hir.load_local  : ptr<element, u128> #[local = [[L]]];
+; CHECK-NEXT:         hir.store [[R2]], v[[#]]
+"#;
+
+    let config = Config {
+        enable_var_scope: false,
+        ..Config::default()
+    };
+    let mut test = Test::new(CHECKS, &config);
+    test.verify(SOURCE)?;
+    Ok(())
+}
+
+#[test]
+#[should_panic = "reference to undefined variable 'L'"]
+fn midenc_hir_example_with_local_bindings_and_var_scoping_enabled() {
+    const SOURCE: &str = "\
+public builtin.function @test::spill_nested_scf_if(v0: ptr<element, u8>) -> u32 {
+^block1(v0: ptr<element, u8>):
+     v2 = hir.ptr_to_int v0 : u32;
+     v3 = arith.constant 32 : u32;
+     v4 = arith.add v2, v3 : u32 #[overflow = unchecked];
+     v5 = hir.int_to_ptr v4 : ptr<element, u128>;
+     v6 = hir.load v5 : u128;
+     v7 = arith.constant 64 : u32;
+     v8 = arith.add v2, v7 : u32 #[overflow = unchecked];
+     v9 = hir.int_to_ptr v8 : ptr<element, u128>;
+     v10 = hir.load v9 : u128;
+     v11 = arith.constant 1 : u64;
+     hir.store_local v5 #[local = lv0];
+     v12 = hir.exec @test/example(v9, v6, v10, v10, v11) : u32
+     v13 = arith.constant 0 : u32;
+     v14 = arith.eq v12, v13 : i1;
+     v15 = scf.if v14 : u32 {
+     ^block3:
+         v18 = hir.load_local  : ptr<element, u128> #[local = lv0];
+         hir.store v18, v4;
+         scf.yield v12;
+     } else {
+     ^block4:
+         v19 = hir.load_local  : ptr<element, u128> #[local = lv0];
+         hir.store v19, v4;
+         scf.yield v12;
+     };
+     cf.br ^block2(v15);
+ ^block2(v1: u32):
+     builtin.ret v1;
+};
+";
+    const CHECKS: &str = r#"; COM: Spill of v5
+; CHECK:     hir.store_local v5 #[local = [[L:lv[0-9]+]]];
+; CHECK-NEXT:     {{v[0-9]+}} = hir.exec @test/example
+
+; COM: First reload of v5
+; CHECK-LABEL:     ^block3
+; CHECK:         [[R1:v[0-9]+]] = hir.load_local  : ptr<element, u128> #[local = [[L]]];
+; CHECK-NEXT:         hir.store [[R1]], v[[#]]
+
+; COM: Second reload of v5
+; CHECK-LABEL:     ^block4
+; CHECK:         [[R2:v[0-9]+]] = hir.load_local  : ptr<element, u128> #[local = [[L]]];
+; CHECK-NEXT:         hir.store [[R2]], v[[#]]
+"#;
+
+    let config = Config {
+        enable_var_scope: true,
+        ..Config::default()
+    };
+    let mut test = Test::new(CHECKS, &config);
+    test.verify(SOURCE).unwrap();
+}
+
+#[test]
+fn midenc_hir_example_with_mixed_bindings_and_var_scoping_enabled() -> DiagResult<()> {
+    const SOURCE: &str = "\
+public builtin.function @test::spill_nested_scf_if(v0: ptr<element, u8>) -> u32 {
+^block1(v0: ptr<element, u8>):
+     v2 = hir.ptr_to_int v0 : u32;
+     v3 = arith.constant 32 : u32;
+     v4 = arith.add v2, v3 : u32 #[overflow = unchecked];
+     v5 = hir.int_to_ptr v4 : ptr<element, u128>;
+     v6 = hir.load v5 : u128;
+     v7 = arith.constant 64 : u32;
+     v8 = arith.add v2, v7 : u32 #[overflow = unchecked];
+     v9 = hir.int_to_ptr v8 : ptr<element, u128>;
+     v10 = hir.load v9 : u128;
+     v11 = arith.constant 1 : u64;
+     hir.store_local v5 #[local = lv0];
+     v12 = hir.exec @test/example(v9, v6, v10, v10, v11) : u32
+     v13 = arith.constant 0 : u32;
+     v14 = arith.eq v12, v13 : i1;
+     v15 = scf.if v14 : u32 {
+     ^block3:
+         v18 = hir.load_local  : ptr<element, u128> #[local = lv0];
+         hir.store v18, v4;
+         scf.yield v12;
+     } else {
+     ^block4:
+         v19 = hir.load_local  : ptr<element, u128> #[local = lv0];
+         hir.store v19, v4;
+         scf.yield v12;
+     };
+     cf.br ^block2(v15);
+ ^block2(v1: u32):
+     builtin.ret v1;
+};
+";
+    const CHECKS: &str = r#"; COM: Spill of v5
+; CHECK:     hir.store_local v5 #[local = [[$L:lv[0-9]+]]];
+; CHECK-NEXT:     {{v[0-9]+}} = hir.exec @test/example
+
+; COM: First reload of v5
+; CHECK-LABEL:     ^block3
+; CHECK:         [[R1:v[0-9]+]] = hir.load_local  : ptr<element, u128> #[local = [[$L]]];
+; CHECK-NEXT:         hir.store [[R1]], v[[#]]
+
+; COM: Second reload of v5
+; CHECK-LABEL:     ^block4
+; CHECK:         [[R2:v[0-9]+]] = hir.load_local  : ptr<element, u128> #[local = [[$L]]];
+; CHECK-NEXT:         hir.store [[R2]], v[[#]]
+"#;
+
+    let config = Config {
+        enable_var_scope: true,
+        ..Config::default()
+    };
+    let mut test = Test::new(CHECKS, &config);
+    test.verify(SOURCE)?;
+    Ok(())
+}
